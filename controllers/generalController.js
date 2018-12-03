489a38513185f1687336eba968cfc03faa6a0b60
Dan-RT <daniel.regnard@efrei.net>
2018-12-03 13:20:58 -0500

    basic notification

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -1,21 +1,81 @@
-var app = angular.module('tennisBet',[]);
+var app = angular.module('tennisBet',['growlNotifications', 'ngAnimate']);
 
 app.controller('generalController', ['$scope', '$http',function($scope, $http) {    
     $scope.runInterval = true;
+    $scope.previousMatchs = [];
+    var flag = false;
 
-    //Make the request every 5s
+    vm = this;
+
+
+	vm.checkSets = function (manches, previousManches, joueur1, joueur2) {
+    	if (manches[0] != previousManches[0]) {
+    		var str = "MANCHE GAGNÉE PAR " + joueur1.prenom + " " + joueur1.nom + "\n"
+    					+ "New score : " + manches[0] + " - " + manches[1] + "\n"
+    						+ "Old score : " + previousManches[0] + " - " + previousManches[1]
+    		
+    		alert(str);
+    		return true;
+    	} else if (manches[1] != previousManches[1]) {
+    		
+    		var str = "MANCHE GAGNÉE PAR " + joueur2.prenom + " " + joueur2.nom + "\n"
+    					+ "New score : " + manches[0] + " - " + manches[1] + "\n"
+    						+ "Old score : " + previousManches[0] + " - " + previousManches[1]
+    		
+    		alert(str);
+
+    		return true;
+    	}
+    	return false;
+    }
+
+    vm.checkMatchs = function (matchs, previousMatchs) {
+
+    	for (i = 0; i < matchs.length; i++) {
+	    	vm.checkSets(matchs[i].pointage.manches, 
+	    					previousMatchs[i].pointage.manches, 
+	    						matchs[i].joueur1, 
+	    							matchs[i].joueur2);
+		}
+		
+    }
+
+
+    vm.fetchParties = function() {
+		console.log("Fetching Data from server...");
+    	$http({
+	        method : "GET",
+	        url : "https://localhost:8000/parties"
+	    }).then(function mySuccess(response) {
+	    	//console.log(response.data);
+	        $scope.matchs = response.data;
+
+	       	/*console.log("$scope.matchs");
+	        console.log($scope.matchs);
+	        console.log("$scope.previousMatchs");
+	        console.log($scope.previousMatchs);*/
+
+	        try {
+	        	vm.checkMatchs($scope.matchs, $scope.previousMatchs);
+	        	console.log("normal assign");
+	        	$scope.previousMatchs = $scope.matchs;
+	        } catch(e) {
+	        	console.log(e);
+	        	console.log("1st assign");
+	        	$scope.previousMatchs = $scope.matchs;
+	        }
+	    }, function myError(response) {
+			console.log(response.statusText);
+		})
+    };
+
+    vm.fetchParties();
+
     setInterval(function () {
     	if($scope.runInterval){
-		    $http({
-		        method : "GET",
-		        url : "https://localhost:8000/parties"							//127.0.0.1
-		    }).then(function mySuccess(response) {
-		        $scope.matchs = response.data;
-		    }, function myError(response) {
-				console.log(response.statusText);
-			})
+		    vm.fetchParties();
 	    }
-	}, 5000);
+	}, 2500);
 
 	$scope.showElement = function (isShown){
 		return !isShown;
@@ -35,6 +95,13 @@ app.controller('generalController', ['$scope', '$http',function($scope, $http) {
 	        url : "https://localhost:8000/parties"							//127.0.0.1
 	    }).then(function mySuccess(response) {
 	        $scope.matchs = response.data;
+
+	        console.log("$scope.matchs");
+	        console.log($scope.matchs);
+	        console.log("$scope.previousMatchs");
+	        console.log($scope.previousMatchs);
+
+	        $scope.previousMatchs = $scope.matchs;
 	    }, function myError(response) {
 			console.log(response.statusText);
 		})
